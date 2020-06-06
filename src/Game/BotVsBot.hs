fd3ea70573f5e2be114f2caa2a8eeea170b85542
Jake Wood <jakew@captricity.com>
2020-06-05 23:49:26 -0700

    Bot vs bot works. very cool

--- src/Game/BotVsBot.hs
+++ src/Game/BotVsBot.hs
@@ -2,12 +2,13 @@ module BotVsBot where
 
 
 import AI.Oracle
+import Control.Monad.Loops
 import Data.List
 import Game.ScrabbleBoard
 import Game.ScrabbleGame
 import System.Random
-import qualified Data.Matrix as Mat
 import qualified Data.Map as M
+import qualified Data.Matrix as Mat
 
 
 type LetterBag = String
@@ -24,28 +25,54 @@ drawFromLetterBag letterBag rack board charCoords = (newLetterBag, newRack)
     newRack = (rack \\ playedLetters) ++ take numLettersPlayed letterBag
     newLetterBag = drop numLettersPlayed letterBag
 
-makePlay :: LetterBag -> Board -> Rack -> IO (Maybe (LetterBag, Board, Rack, Score))
-makePlay letterBag board rack =
+makePlay :: (LetterBag, Board, Rack) -> IO (LetterBag, Board, Rack)
+makePlay (letterBag, board, rack) =
   if genericLength rack == 0
-    then return Nothing
+    then return (letterBag, board, rack)
   else do
     rankedPlayspots <- oracle board rack
     if genericLength rankedPlayspots == 0
-      then return Nothing
+      then return (letterBag, board, rack)
     else do
       let (word, score, coords) = head rankedPlayspots
           charCoords = zip word coords
           newBoard = applyPlayspot board charCoords
           (newLetterBag, newRack) = drawFromLetterBag letterBag rack board charCoords
-      return (Just (newLetterBag, newBoard, newRack, score))
+      return (newLetterBag, newBoard, newRack)
+
+startGame :: StdGen -> IO (LetterBag, Board, Rack)
+startGame g =
+  let
+    letterBag = initLetterBag g
+    (p1Rack, letterBag1) = genericSplitAt 7 letterBag
+    (p2Rack, letterBag2) = genericSplitAt 7 letterBag1
+  in makePlay (letterBag1, startBoard, p1Rack)
 
 
-playGame :: StdGen -> IO (Maybe Board)
-playGame g = do
-  let letterBag = initLetterBag g
-      (p1Rack, letterBag1) = genericSplitAt 7 letterBag
-      (p2Rack, letterBag2) = genericSplitAt 7 letterBag1
-  play <- makePlay letterBag1 startBoard p1Rack
-  case play of
-    Nothing -> return Nothing
-    Just (l, b , r, s) -> return (Just b)
+playGame :: IO ()
+playGame = do
+  g <- newStdGen
+  (l, b, r) <- startGame g >>= makePlay >>= makePlay >>= makePlay >>= makePlay
+  putStr . show $ easyReadBoard b
+
+playGame' :: IO ()
+playGame' = do
+  g <- newStdGen
+  game@(l, b, r) <- startGame g
+  game@(l, b, r) <- makePlay game
+  game@(l, b, r) <- makePlay game
+  game@(l, b, r) <- makePlay game
+  putStr . show $ easyReadBoard b
+
+isGameOver :: (LetterBag, Board, Rack) -> Bool
+isGameOver (l, _, _) = genericLength l == 0
+
+playGame'' :: IO ()
+playGame'' = do
+  g <- newStdGen
+  start <- startGame g
+  (_, b, _) <- iterateUntilM isGameOver makePlay start
+  putStr . show $ easyReadBoard b
+
+main :: IO ()
+main = playGame''
